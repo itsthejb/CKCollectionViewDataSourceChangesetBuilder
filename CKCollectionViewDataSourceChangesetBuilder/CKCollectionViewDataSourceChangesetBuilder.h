f16a720100335d1c5f5b7fb16a2c238d5c87a4dc
Jonathan Crooke <joncrooke@gmail.com>
2015-10-07 17:08:48 +0200

    Initial Commit

---
+++ CKCollectionViewDataSourceChangesetBuilder/CKCollectionViewDataSourceChangesetBuilder.h
@@ -0,0 +1,13 @@
+//
+//  CKCollectionViewDataSourceChangesetBuilder.h
+//  CKCollectionViewDataSourceChangesetBuilder
+//
+//  Created by Jonathan Crooke on 07/10/2015.
+//  Copyright © 2015 Jonathan Crooke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CKCollectionViewDataSourceChangesetBuilder : NSObject
+
+@end
